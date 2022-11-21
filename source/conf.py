11c34cbe1a5e8ab74cf19f2370a53095a0a297a8
Madeleine van Winkel <madeleine.vanwinkel@nelen-schuurmans.nl>
2022-11-21 16:25:10 +0100

    Preparing release 2022.41

--- source/conf.py
+++ source/conf.py
@@ -5,7 +5,7 @@ import sphinx_rtd_theme
 # The 3Di release name that's shown at the top of the sidebar.
 # Note: non-production documentation builds are marked as such, you don't need
 # to change the release name for that.
-THREEDI_RELEASE = "2022-02 Klondike Release"
+THREEDI_RELEASE = "2022-03 Klondike Release"
 
 
 # -- General configuration ------------------------------------------------
